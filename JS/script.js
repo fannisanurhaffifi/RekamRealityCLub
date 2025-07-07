47e4566879ad981d35be11bafdf515bc1745a936
fannisanurhaffifi <166141571+fannisanurhaffifi@users.noreply.github.com>
2025-07-07 12:06:45 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik diluar sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
